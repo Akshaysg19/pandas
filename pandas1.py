40a1a106a9609b03cfdcbfda6e6b1b2203923df2
Akshaysg19 <akshaysgujjar@gmail.com>
2024-08-26 19:11:02 +0530

    "NEW ENTRY FOR PANDAS"

--- pandas1.py
+++ pandas1.py
@@ -1,34 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-'''#### Series ####
 
-a = [1,2,3,4]
-
-op = pd.Series(a, index=['a','b','c','d'])
-#print(op)
-#print(op[1])
-#print(op['a'])
-
-# dict to series
-calories = {"day1": 420, "day2": 380, "day3": 390}
-cal_op = pd.Series(calories)
-print(cal_op)
-
-#DataFrame to Series
-data = {
-  "calories": [420, 480, 490],
-  "duration": [50, 40, 45]
-}
-
-call_op = pd.Series(data['calories'], index=data['duration'])
-print(call_op)
-
-cal_dataframe = pd.DataFrame(data)
-print(cal_dataframe.loc[0])
-
-cal_dataframe_index = pd.DataFrame(data, index=['a','b','c'])
-print(cal_dataframe_index.loc['b'])'''
 
 #Reading json file
 json_data = pd.read_json('data.json')
